421665c2a702d2a11ae6d993394468c71b017219
MariyaKor <korotmarie@gmail.com>
2022-12-17 22:51:38 +0300

    lesson 22 final commit

--- lesson22/src/main/java/com/tms/DemoServlet.java
+++ lesson22/src/main/java/com/tms/DemoServlet.java
@@ -1,7 +1,16 @@
 package com.tms;
 
-public class DemoServlet {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.println("Hello from DemoServlet! ");
+        outputStream.close();
     }
 }
